2b2f86d2507e725f72618c451df7a0e6192ac395
N Varun Reddy <nvarunreddy190397@gmail.com>
2021-08-12 19:56:46 -0400

    Code to load wishlisted or booked hotels booked hotels from indexedDB and display them

--- TeamAlfa_hotelbook_FP/manage/manage.js
+++ TeamAlfa_hotelbook_FP/manage/manage.js
@@ -6,6 +6,134 @@ window.onload = () => {
         document.getElementById("logout").innerHTML="Login"
 
     }
+    let request = window.indexedDB.open("hotels")
+        , db, tx3, storemanage, indexmanage, tx2, storewishlist, indexwishlist
+        ;
+    window.indexedDB = window.indexedDB || window.mozIndexedDB ||
+        window.webkitIndexedDB || window.msIndexedDB;
+
+
+    request.onupgradeneeded = (e) => {
+
+    }
+
+    request.onerror = (e) => {
+        console.log('error is comming' + e.target.errorCode);
+    }
+
+
+    request.onsuccess = (e) => {
+        db = request.result;
+        tx2 = db.transaction("manage", "readwrite");
+        storewishlist = tx2.objectStore("manage");
+        indexwishlist = storewishlist.index("title");
+
+
+
+
+        db.onerror = (e) => {
+            console.log('error is comming' + e.target.errorCode);
+        }
+
+
+        let items = storewishlist.getAll();
+
+
+        items.onsuccess = () => {
+            tx3 = db.transaction("checkin", "readwrite");
+            storemanage = tx3.objectStore("checkin");
+            indexmanage = storemanage.index("id");
+            let s1 = items.result.length;
+
+
+            for (let i = 0; i < s1; i++) {
+                let item2 = indexmanage.get(items.result[i].id);
+                item2.onsuccess = () => {
+                    if (items.result[i].mail == localStorage.getItem("mail")) {
+                        var html = `
+                <div class="manage-image">
+                    <img src="${items.result[i].images}"/>
+                </div>
+                <div class="manage-detail-container">
+                    <div class="manage-detail-box">
+                        <div class="manage-name">
+                            <b>${items.result[i].title}</b>
+                            <span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star"></span>
+                                <span class="fa fa-star"></span>
+                            </span>
+                        </div>
+                        <div class="manage-location">
+                        ${items.result[i].city}
+
+                        </div>
+                    </div>
+                    <div class="manage-checkroom-box">
+                        <div class="manage-info-container">
+                            <div class="manage-include-icon">
+                            <span class="fa fa-wifi"></span>
+                            <span><i class="material-icons">restaurant</i></span>
+                            <span><i class="material-icons">local_parking</i></span>
+                            </div>
+                            <ul>
+                            ${items.result[i].services.map((item) => {
+                            return (
+                                `<li>
+                ${item}
+                </li>`
+                            )
+                        }).join('')}
+                            </ul>
+                        </div>
+                        <div class="manage-price">
+                            <div class="manage-price-off">
+                            ${items.result[i].price}
+
+                            </div>
+                            <div class="manage-offer">
+                                <span>
+                                ${items.result[i].originalprice}
+
+                                </span>
+                                <span>
+                                ${100 - Math.floor(parseInt(items.result[i].price) * 100 / parseInt(items.result[i].originalprice))}
+                                %off
+                                </span>
+                            </div>
+                            <div class="manage-date">
+                                <span>From:${item2.result.from}</span>
+                                <span>To:${item2.result.to
+
+                            }</span>
+                            </div>
+                            
+                            <button class="bin2" id="bin">Cancel Booking</button>
+                        </div>
+                    </div>
+                </div>
+        `;
+
+                        var wrapper = document.createElement('div');
+                        wrapper.innerHTML = html;
+                        wrapper.setAttribute('class', 'manage-box')
+
+                        document.getElementById('manage-container').appendChild(wrapper);
+                        document.getElementsByClassName("bin2")[i].onclick = () => {
+                            del(items.result[i].uid);
+                            window.location.reload();
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
 }
 
 
