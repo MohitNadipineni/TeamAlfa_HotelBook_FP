5c5d3f39d75657fbd4924ff79b790b44f63bf3f7
Aman Verma <amanvr2@gmail.com>
2021-08-12 17:53:16 -0400

    Code to Update the IndexedDB on Clicking Login button
    Code to Update the IdexedDB on Clicking Login button

--- TeamAlfa_hotelbook_FP/login/login.js
+++ TeamAlfa_hotelbook_FP/login/login.js
@@ -17,7 +17,40 @@ document.getElementById('login-btn').onclick = () => {
     }
 
 
-   
+    request.onsuccess = (e) => {
+        db = request.result;
+        tx = db.transaction("user", "readwrite");
+        store = tx.objectStore("user");
+        index = store.index("mail");
+
+
+
+        db.onerror = (e) => {
+            console.log('error is comming' + e.target.errorCode);
+        }
+
+
+        let items = store.getAll();
+        let y = false;
+
+        items.onsuccess = () => {
+            let s1 = items.result.length;
+
+            for (let i = 0; i < s1; i++) {
+                if (mail == items.result[i].mail && pass == items.result[i].pass) {
+                    localStorage.setItem("mail", mail);
+                    localStorage.setItem("login","true");
+                    y = true;
+                    window.location = '../index.html';
+                }
+
+            }
+            if (!y) {
+                alert("your credentials is wrong please enter right credentials")
+            }
+        }
+
+    }
 
 }
 
