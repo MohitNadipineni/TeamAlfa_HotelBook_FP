9b16120cdc16f571a16da9e0867b899c4e05c6c0
Aman Verma <amanvr2@gmail.com>
2021-08-12 18:11:10 -0400

    Validating All Fields of a form

--- TeamAlfa_hotelbook_FP/register/register.js
+++ TeamAlfa_hotelbook_FP/register/register.js
@@ -29,3 +29,61 @@ document.getElementById('clear-btn').onclick = () => {
     document.getElementById('reg-pass2').value = "";
     document.getElementById('reg-comment').value = "";
 }
+document.getElementById('reg-btn').onclick = () => {
+    let mail = document.getElementById('reg-mail').value;
+    let url = document.getElementById('reg-url').value;
+    let date = document.getElementById('reg-date').value;
+    let pass = document.getElementById('reg-pass').value;
+    let pass2 = document.getElementById('reg-pass2').value;
+    let comment = document.getElementById('reg-comment').value;
+
+
+    if (mail == "" || url == "" || date == "" || pass == "" || pass2 == "" || comment == "") {
+        cont = false;
+        alert("please fill all fields");
+        return;
+    }
+
+    if (pass != pass2) {
+        cont = false;
+        document.getElementById('valid-pass').innerHTML = "*password do not match"
+        return;
+    }
+    else if (pass == pass2) {
+        document.getElementById('valid-pass').innerHTML = ""
+    }
+    if (pass.length < 7) {
+        alert("please enter pass of length greater than 6");
+        cont = false;
+        return;
+    }
+
+    ValidateEmail(mail);
+
+    if (mail != "" && url != "" && date != "" && pass != "" && pass2 != "" && comment != "" && pass == pass2 && ValidateEmail(mail) == true && pass > 6) {
+        cont = true;
+    }
+
+
+    if (cont == true) {
+
+        let val = {
+            mail: mail, url: url, date: date, pass: pass, comment: comment, gender: $("input[type='radio'][name='gender']:checked").val()
+        }
+
+
+        add(val);
+
+    }
+
+
+}
+
+function ValidateEmail(mail) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+        return (true)
+    }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+
